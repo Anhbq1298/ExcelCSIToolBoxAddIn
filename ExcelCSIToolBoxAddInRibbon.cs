98f03b098768fe8b5f98565965a1b159d40425da
aBui <buiquanganh1298@gmail.com>
2026-04-12 23:13:36 +0700

    Add project files.

---
+++ ExcelCSIToolBoxAddInRibbon.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
+
+
+namespace ExcelCSIToolBoxAddIn
+{
+    public partial class ExcelCSIToolBoxAddInRibbon
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+           MessageBox.Show("Hello World!");
+        }
+    }
+}
