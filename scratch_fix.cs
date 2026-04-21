69ed8f1fdffde40f860f57e6acbae158481bf619
aBui <buiquanganh1298@gmail.com>
2026-04-21 10:03:10 +0700

    feat: add EtabsToolboxWindow UI and auxiliary fix utilities

---
+++ scratch_fix.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string path = @"c:\repo\ExcelCSIToolBoxAddIn\UI\Views\EtabsToolboxWindow.xaml";
+        string content = File.ReadAllText(path);
+        string pattern = @"Content=""(Run|By UniqueName|By Coor|By Point|Get|Set)""\s+Foreground=""Blue""";
+        string replacement = "Content=\"$1\"\r\n                                    HorizontalAlignment=\"Stretch\"\r\n                                    Foreground=\"Blue\"";
+        content = Regex.Replace(content, pattern, replacement);
+        File.WriteAllText(path, content);
+    }
+}
